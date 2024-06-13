587b72cef185560c6114f82a7ed373dc8e2e0506
Thiela1 <Thiela1@ferris.edu>
2024-06-13 02:22:04 +0000

    //Pizza Thickness and Flavored crust

--- PizzaCrust.java
+++ PizzaCrust.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class PizzaCrust {
 	int crustPref;
-	int userInput;// added a comment
+	int userInput;
+	double thickness;
+	String crustFlavor;// added a comment
 	
 	// Allows customer to choose their style of crust
 	// Adding comment to test conflict
@@ -26,7 +28,41 @@ public class PizzaCrust {
 		else {
 			System.out.println("Not a valid input.");
 		}
+	}
+	// Allows customer to choose custom crust thickness
+	private void chooseThickness() {
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Enter your desired crust thickness (in inches, e.g., 0.5, 1.0, 1.5): ");
+		thickness = scnr.nextDouble();
+		System.out.println("You've chosen a crust thickness of " + thickness + " inches.");
+	}
+	
+	// Allows customer to choose crust flavoring
+	private void chooseCrustFlavor() {
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Choose your crust flavoring: \n 1. Garlic\n 2. Herb\n 3. Cheese\n 4. No flavor");
+		userInput = scnr.nextInt();
 		
+		switch (userInput) {
+			case 1:
+				crustFlavor = "Garlic";
+				System.out.println("You've chosen Garlic flavoring.");
+				break;
+			case 2:
+				crustFlavor = "Herb";
+				System.out.println("You've chosen Herb flavoring.");
+				break;
+			case 3:
+				crustFlavor = "Cheese";
+				System.out.println("You've chosen Cheese flavoring.");
+				break;
+			case 4:
+				crustFlavor = "No flavor";
+				System.out.println("You've chosen no flavoring.");
+				break;
+			default:
+				System.out.println("Not a valid input.");
+				crustFlavor = "No flavor"; // Default option if invalid input
 		//Adding comment
 		//Another comment
 		
